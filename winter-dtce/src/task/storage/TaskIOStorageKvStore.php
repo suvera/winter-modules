96a688020dc6bf1218507f62b8928695972134e0
suvera <rana.ram@gmail.com>
2021-07-02 18:47:04 +0530

    Code fixes

--- winter-dtce/src/task/storage/TaskIOStorageKvStore.php
+++ winter-dtce/src/task/storage/TaskIOStorageKvStore.php
@@ -5,8 +5,7 @@ namespace dev\winterframework\dtce\task\storage;
 
 use dev\winterframework\core\context\ApplicationContext;
 use dev\winterframework\dtce\exception\DtceException;
-use dev\winterframework\io\kv\KvClient;
-use dev\winterframework\io\kv\KvConfig;
+use dev\winterframework\io\kv\KvTemplate;
 use dev\winterframework\io\stream\InputStream;
 use dev\winterframework\io\stream\StringInputStream;
 use dev\winterframework\util\log\Wlf4p;
@@ -17,8 +16,7 @@ class TaskIOStorageKvStore implements TaskIOStorageHandler {
 
     const PREFIX = 'winter-dtce-io-';
 
-    protected KvConfig $config;
-    protected KvClient $client;
+    protected KvTemplate $client;
     protected string $domain;
     private int $ttl;
     protected int $retries;
@@ -28,25 +26,15 @@ class TaskIOStorageKvStore implements TaskIOStorageHandler {
         protected array $taskDef
     ) {
         $port = $this->ctx->getPropertyInt('winter.kv.port', 0);
-        $address = $this->ctx->getPropertyStr('winter.kv.address', '');
         if ($port <= 0) {
             throw new DtceException('Shared KV server is not setup to use KV Store');
         }
+
+        $this->client = $this->ctx->beanByClass(KvTemplate::class);
         $this->ttl = $this->taskDef['storage.kv.ttl'] ?? self::GC_TIME;
         $this->retries = 5;
 
-        $this->config = new KvConfig(
-            $port,
-            $address ?: null,
-            null
-        );
         $this->domain = self::PREFIX . $this->taskDef['name'];
-    }
-
-    protected function getClient(): void {
-        if (!isset($this->client)) {
-            $this->client = new KvClient($this->config);
-        }
     }
 
     public function getInputStream(int|string $dataId): InputStream {
@@ -54,8 +42,6 @@ class TaskIOStorageKvStore implements TaskIOStorageHandler {
         $value = '';
         while ($i > 0) {
             try {
-                $this->getClient();
-
                 $value = $this->client->get($this->domain, '' . $dataId);
 
                 if (is_null($value)) {
@@ -75,7 +61,6 @@ class TaskIOStorageKvStore implements TaskIOStorageHandler {
         $i = $this->retries;
         while ($i > 0) {
             try {
-                $this->getClient();
                 $this->client->put($this->domain, $dataId, '' . $data, $this->ttl);
             } catch (Throwable $e) {
                 self::logException($e);
@@ -89,7 +74,6 @@ class TaskIOStorageKvStore implements TaskIOStorageHandler {
         $i = $this->retries;
         while ($i > 0) {
             try {
-                $this->getClient();
                 $this->client->del($this->domain, '' . $dataId);
             } catch (Throwable $e) {
                 self::logException($e);
